4b42aab7a3cc8d8e50a60db112b15467f3405534
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-05-02 14:44:21 +0300

    Component Life Cycle Hook ornekleri ve temizlik

--- antrenman/src/app/app.component.ts
+++ antrenman/src/app/app.component.ts
@@ -1,32 +1,28 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { randomInt } from 'crypto';
 import { TiestoComponent } from './tiesto/tiesto.component';
-import { DomatComponent } from './domat/domat.component';
 import { ParentComponent as ParentComponent_PTC } from './components/parent-to-child/parent/parent.component';
 import { ParentComponent as ParentComponent_CTP } from './components/child-to-parent/parent/parent.component';
 import { ParentCtcComponent } from './components/child-to-child/parent-ctc/parent-ctc.component';
+import { DomatComponent } from './domat/domat.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
     <hr />
-    <app-parent> </app-parent>
-    <app-parent-CTP> </app-parent-CTP>
-    <app-parent-ctc> </app-parent-ctc>
+    <app-domat data="deneme"> Bu domatin contenti </app-domat>
   `,
-  styleUrl: './app.component.css',
   imports: [
     RouterOutlet,
     NgFor,
     CommonModule,
     TiestoComponent,
-    DomatComponent,
     ParentComponent_PTC,
     ParentComponent_CTP,
     ParentCtcComponent,
+    DomatComponent,
   ],
 })
 export class AppComponent {
--- antrenman/src/app/domat/domat.component.ts
+++ antrenman/src/app/domat/domat.component.ts
@@ -1,39 +1,70 @@
 import {
-  CurrencyPipe,
-  DatePipe,
-  DecimalPipe,
-  JsonPipe,
-  KeyValuePipe,
-  PercentPipe,
-  SlicePipe,
-  TitleCasePipe,
-  UpperCasePipe,
-} from '@angular/common';
-import { Component } from '@angular/core';
-import { CustomPipe } from '../pipes/custom.pipe';
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-domat',
   standalone: true,
-  imports: [
-    TitleCasePipe,
-    SlicePipe,
-    CurrencyPipe,
-    DatePipe,
-    JsonPipe,
-    PercentPipe,
-    DecimalPipe,
-    KeyValuePipe,
-    UpperCasePipe,
-    CustomPipe,
-  ],
-  templateUrl: './domat.component.html',
-  styleUrl: './domat.component.css',
+  imports: [FormsModule],
+  template: `
+    domatzzz show
+    <br />
+    <input type="text" [(ngModel)]="data" />
+    <br />
+    <ng-content></ng-content>
+  `,
 })
-export class DomatComponent {
-  constructor(private custom: CustomPipe) {
-    console.log(custom.transform('denemedeneme', 2, 5));
+export class DomatComponent
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  @Input() data: any;
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(' 1. adim  ILKK ILK ngOnChanges tetiklendi');
+    console.log(changes);
   }
-  name = 'cagdas';
-  price = 12345;
+  ngOnInit(): void {
+    console.log('2. adim ngOnInIt calistii');
+  }
+  ngDoCheck(): void {
+    console.log('3.adim Docheck calistii birden cok calisabiliyor');
+    //componentin stateinde vs bir degisim oldugunda tetikleniyor.
+  }
+  ngAfterContentInit(): void {
+    console.log('4. Aftercontentinit');
+    //content gonderildiginde tetiklenicek olan fonksiyon
+  }
+  ngAfterContentChecked(): void {
+    console.log('5. tetiklenicek olan aftercontentcheck');
+    //docheck ile ayni anda tetikleniyor. docheck butun degisikliklerde tetiklenirken aftercontentchech ise contentteki degisimde
+  }
+  ngAfterViewInit(): void {
+    console.log('6. tetiklenen ngafterviewinit');
+    //tamamiyle templatein handle edildiginde bu tetikleniyor.
+  }
+  ngAfterViewChecked(): void {
+    console.log('7.tetiklenen AfterViewChecked');
+    // bu event viewde bir degisiklik varsa tetikleniyor
+  }
+  ngOnDestroy(): void {
+    console.log('8. tetiklenen ON DESTROYY');
+  }
 }
