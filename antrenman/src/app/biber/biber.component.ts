2e8bb6e958f453ad3b9d19c0e8e2690e564e2258
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-05-04 15:39:00 +0300

    form state calismasi. stateleri incelemek ve manuple etmek icin.

--- antrenman/src/app/biber/biber.component.ts
+++ antrenman/src/app/biber/biber.component.ts
@@ -24,8 +24,42 @@ import {
       </div>
       <button>Send</button>
     </form>
-    isValid:{{ frm.valid }}
+    <div>isValid:{{ frm.valid }}</div>
     <button (click)="change()">Change</button>
+    <br />
+    <button (click)="markAsTouched()">markAsTouched</button>
+    <br />
+    <button (click)="markAllAsTouched()">markAllAsTouched</button>
+    <br />
+    <button (click)="markAsUntouched()">markAsUntouched</button>
+    <br />
+    <button (click)="markAsDirty()">markAsDirty</button>
+    <br />
+    <button (click)="markAsPristine()">markAsPristine</button>
+    <br />
+    <button (click)="disable()">disable email</button>
+    <br />
+    <button (click)="enable()">enable email</button>
+    <div style="border: 1px solid black; width:250px">
+      <div style="border: 1px solid black;">
+        form touched: {{ frm.touched }} <br />
+        FirstName form control touched:
+        {{ frm.get('firstName').touched }} <br />
+        adress group from touched: {{ frm.get('adress').touched }} <br />
+        Country from control touched:
+        {{ frm.get('adress').get('country').touched }}
+      </div>
+      <div style="border: 1px solid black;">
+        form dirty:{{ frm.dirty }}
+        <br />
+        firstName form control dirty: {{ frm.get('firstName').dirty }}
+      </div>
+      <div style="border: 1px solid black;">
+        form pristine:{{ frm.pristine }}
+        <br />
+        firstName form control pristine: {{ frm.get('firstName').pristine }}
+      </div>
+    </div>
   `,
 })
 export class BiberComponent {
@@ -62,5 +96,27 @@ export class BiberComponent {
   change() {
     this.frm.get('firstName').setValue('cagdas', { onlySelf: true });
     this.frm.get('email').setValue('cds@gma.com', { onlySelf: true });
+  }
+
+  enable() {
+    this.frm.get('email').enable();
+  }
+  disable() {
+    this.frm.get('email').disable();
+  }
+  markAsPristine() {
+    this.frm.markAsPristine();
+  }
+  markAsDirty() {
+    this.frm.markAsDirty();
+  }
+  markAsUntouched() {
+    this.frm.markAsUntouched();
+  }
+  markAllAsTouched() {
+    this.frm.markAllAsTouched();
+  }
+  markAsTouched() {
+    this.frm.get('firstName').markAsTouched({ onlySelf: true });
   }
 }
