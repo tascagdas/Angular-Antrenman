d447f493f49c5883f752066bf4e4e7a4859180ef
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-10-22 15:28:50 +0300

    for dongusu eski ve yeni kullanimi

--- antrenman/src/app/orman-meyvesi/orman-meyvesi.component.ts
+++ antrenman/src/app/orman-meyvesi/orman-meyvesi.component.ts
@@ -9,6 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './orman-meyvesi.component.css'
 })
 export class OrmanMeyvesiComponent {
-
+  numbers : number[] = [1,2,3,4,5,6,7,8,9,10]
   condition : boolean = true;
 }
